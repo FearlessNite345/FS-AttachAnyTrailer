07c2d5a39d49821eb2cbb28e0a2917b5d775c984
FearlessNite345 <Joshualambert345@gmail.com>
2025-01-20 15:50:49 -0500

    1.0.0

--- client/AttachAnyTrailer.lua
+++ client/AttachAnyTrailer.lua
@@ -9,12 +9,12 @@ CreateThread(function()
                 if GetEntityBoneIndexByName(Vehicle, 'attach_female') ~= -1 then
                     local closestTrailer = FS_Lib:GetClosestTrailerToHitch(config.maxDistance)
                     if closestTrailer ~= nil and closestTrailer ~= 0 then
-                        if config.autoAttachToTrailer then
+                        if config.autoAttachHitchToTrailer then
                             AttachVehicleToTrailer(Vehicle, closestTrailer, 1.2)
                         else
                             updateRate = 0
-                            FS_Lib:DrawText2D(0.5, 0.8, '~g~[' .. FS_Lib:GetKeyString(config.attachTrailerKeybind) .. ']~w~ '  .. config.locales['attachToTrailer'], 0.6, true)
-                            if IsControlJustPressed(0, config.attachTrailerKeybind) then
+                            FS_Lib:DrawText2D(0.5, 0.8, '~g~[' .. FS_Lib:GetKeyString(config.attachKeybind) .. ']~w~ '  .. config.locales['attachToTrailer'], 0.6, true)
+                            if IsControlJustPressed(0, config.attachKeybind) then
                                 AttachVehicleToTrailer(Vehicle, closestTrailer, 1.2)
                             end
                         end
